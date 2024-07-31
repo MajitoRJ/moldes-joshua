7c1772d8e3bdd859d4933f41084b46933fef0e12
MajitoRJ <majolaloca41@gmail.com>
2024-07-31 12:53:07 -0600

    add banner1 slider banner2 and footer

---
+++ script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.bx-chevron-right',
+	  prevEl: '.bx-chevron-left'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 3,
+		spaceBetween: 50,
+	  },
+	} 
+    });
